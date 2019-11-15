418d2722cc4c1711830ec50d590367efb1f13899
Wade Butler <wadebutler2@gmail.com>
2019-11-15 12:37:01 -0500

    refactored exsisting code to be more legible, added win condition and refresh button

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const cards = document.querySelectorAll(".card");
-
 let flippedCard = false;
 let boardLock = false;
+let score = 0;
 let firstCard;
 let secondCard;
 
 // adds flip class to html which flips the cards
 function flipCard() {
-    if (boardLock === true) {return} 
-    if (this === firstCard) {return}
-
+    debug()
     this.classList.add("flip");
 
     if (flippedCard === false) {
@@ -22,22 +20,42 @@ function flipCard() {
         if (firstCard.dataset.number === secondCard.dataset.number) {
             firstCard.removeEventListener("click", flipCard);
             secondCard.removeEventListener("click", flipCard);
+            scoreControl()
+            resetCards();
 
-            resetBoard();
         } else {
-            boardLock = true;
-
-            setTimeout(() => {
-                firstCard.classList.remove("flip");
-                secondCard.classList.remove("flip");
-
-                resetBoard();
-            }, 1500);
+            missMatchReset();
         }
     }
 }
 
-function resetBoard() {
+function debug() {
+    if (boardLock === true) { return }
+    if (this === firstCard) { return }
+}
+
+function missMatchReset() {
+    boardLock = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+
+        resetCards();
+    }, 1500);
+}
+
+function scoreControl() {
+    score++;
+
+    document.querySelector(".score").textContent = score;
+
+    if (score === 4) {
+        console.log("you win!");
+    }
+}
+
+function resetCards() {
     flippedCard = false;
     boardLock = false;
     firstCard = null
@@ -52,3 +70,13 @@ function shuffle() {
 }
 
 cards.forEach(card => card.addEventListener("click", flipCard))
+
+$(document).ready(function () {
+    shuffle();
+
+    // RESET BUTTON
+    $(".reset").click(() => {
+        $(".card").removeClass("flip");
+        shuffle();
+    })
+});
